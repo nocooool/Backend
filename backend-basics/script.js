d20e0c9c3f622039ec955218b8a86bb99d48f0b7
nocooool <nakulshah3932@gmail.com>
2024-07-25 02:20:29 +0530

    ejs files and routing

--- backend-basics/script.js
+++ backend-basics/script.js
@@ -1,5 +1,13 @@
+const { name } = require('ejs');
 const express = require ('express');
 const app = express();
+
+//ejs is template engine used by node.js. it helps to create an html template with minimal code
+//ejs gives dynamic approach to html
+app.set("view engine", "ejs");
+
+//configure the static files
+app.use(express.static('./public'))
 
 //req - contains the user data who sends the request in this case it's me
 //res - contains the user data who sends the respond in this case the server
@@ -7,18 +15,28 @@ const app = express();
 //middleware - this the code that is executed before any code on any route 
 //app.use requires a fucntion as an arg
 //next() - when the control goes to middleware then it requires next() to move to further route
-app.use(function(req, res, next) {
-    console.log("middleware working"); 
-    //it will print output whenever one switches the route 
-    next();
-})
+// app.use(function(req, res, next) {
+//     console.log("middleware working"); 
+//     //it will print output whenever one switches the route 
+//     next();
+// }) --> this is middleware
 
 app.get('/', function (req, res) {
-    res.send('hello world')
+    res.render('index', {name: "AT PraRoz"})
+})
+
+app.get('/contact-us', function(req, res){
+    res.render('contact', {phone: "+911122334455"})
 })
 
 app.get('/profile', function (req, res) {
     res.send('this is the profile')
+})
+
+//as the user is the info from the user side do req.params.user
+//:user is the params (parameter)
+app.get('/profile/:user' , function(req, res){
+    res.send(`welcome to the profile page ${req.params.user}`)
 })
 
 app.listen(3000);
